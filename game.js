b114e196467e20d8be6c2a3c468d268697a5fc43
OmKharade <133861833+OmKharade@users.noreply.github.com>
2023-12-02 16:34:07 -0500

    complete drawWalls()

--- game.js
+++ game.js
@@ -9,6 +9,9 @@ let createRect = (x,y,width,height,color) =>{
 let fps = 30
 let blockSize = 20
 
+let wallSpaceWidth = blockSize/1.5
+let wallOffset = (blockSize - wallSpaceWidth)/2
+
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
@@ -54,10 +57,22 @@ let gameInterval = setInterval(gameLoop, 1000/fps)
 let drawWalls = () => {
     for(let i=0;i<map.length;i++){
         for(let j=0;j<map[0].length;j++){
-            if(map[i][j] == 1){
+            if(map[i][j] == 1){ // solid wall
                 createRect(j*blockSize, i*blockSize, blockSize, blockSize, "#342DCA")
             }
-
+            // innerWall -> black
+            if(j>0 && map[i][j-1]==1){  // right wall
+                createRect(j*blockSize, i*blockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, "black")
+            } 
+            if(j<map[0].length-1 && map[i][j+1] == 1){ // left wall
+                createRect(j*blockSize + wallOffset, i*blockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, "black")
+            }
+            if(i>0 && map[i-1][j]==1){ // top wall
+                createRect(j*blockSize + wallOffset, i*blockSize, wallSpaceWidth, wallSpaceWidth + wallOffset, "black")
+            } 
+            if(i<map.length-1 && map[i+1][j] == 1){ // bottom wall
+                createRect(j*blockSize + wallOffset, i*blockSize + wallOffset, wallSpaceWidth, wallSpaceWidth + wallOffset, "black")
+            }
         }
     }
 }
