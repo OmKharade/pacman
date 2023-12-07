b2f5f23526f2b7e370d974c0897e49bea8f65751
OmKharade <133861833+OmKharade@users.noreply.github.com>
2023-12-06 23:44:18 -0500

    add ghosts

--- game.js
+++ game.js
@@ -9,6 +9,8 @@ let createRect = (x,y,width,height,color) =>{
 let fps = 30
 let blockSize = 20
 let score = 0
+let ghosts = []
+let ghostLocations = [{x:0,y:0},{x:176,y:0},{x:0,y:121},{x:176,y:121},]
 
 let wallSpaceWidth = blockSize/1.5
 let wallOffset = (blockSize - wallSpaceWidth)/2
@@ -68,6 +70,11 @@ let drawScore = () => {
     canvasContext.fillStyle = "white"
     canvasContext.fillText("Score : " +score,0,blockSize*(map.length+1)+10)
 }
+let drawGhosts = () => {
+    for(let i=0;i<ghosts.length;i++){
+        ghosts[i].draw()
+    }
+}
 
 let draw = () => {
     createRect(0,0, canvas.width, canvas.height,"black")
@@ -75,6 +82,7 @@ let draw = () => {
     food()
     pacman.draw()
     drawScore()
+    drawGhosts()
 }
 
 let gameInterval = setInterval(gameLoop, 1000/fps)
@@ -106,7 +114,16 @@ let createNewPacman = () =>{
     pacman = new Player(blockSize,blockSize,blockSize,blockSize,blockSize/5)
 }
 
+let createGhosts = () => {
+    ghosts = []
+    for (let i = 0; i<4; i++){
+        let newGhost = new Ghost(9 * blockSize + ( i % 2 == 0? 0:1)*blockSize, 10 * blockSize + ( i % 2 == 0? 0:1)*blockSize, blockSize, blockSize, pacman.speed/2,ghostLocations[i%4].x,ghostLocations[i%4].y,124,116,6+i)
+        ghosts.push(newGhost)
+    }
+}
+
 createNewPacman()
+createGhosts()
 gameLoop()
 
 window.addEventListener("keydown", (e)=>{
